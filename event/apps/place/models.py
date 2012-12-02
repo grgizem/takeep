a45fdaa14de28148a051e603232f4de778aa12b3
Maximilian Ciceu <maxciceu@yahoo.com>
2012-12-02 11:31:28 +0200

    removed extra lines

--- event/apps/place/models.py
+++ event/apps/place/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 
 class Place(models.Model):
     name = models.CharField(max_length='100')
     purpose = models.CharField(max_length='255')
-#    coordinates = LocationField(blank=True, max_length=255)
+#    coordinates= LocationField(blank=True, max_length=255)
 
     def upload_to(self, filename):
             return 'organizations/%s/%s' % (self.name, filename)
