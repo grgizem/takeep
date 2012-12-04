edea092dd51eac4b07a2ffd2b9c678e484b7e9dc
Maximilian Ciceu <maxciceu@yahoo.com>
2012-12-04 15:36:17 +0200

    added coordinate as CharField

--- event/apps/place/models.py
+++ event/apps/place/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Place(models.Model):
     name = models.CharField(max_length='100')
     purpose = models.CharField(max_length='255')
-#    coordinates= LocationField(blank=True, max_length=255)
+    coordinates = models.CharField(blank=True, max_length=100)
 
     def upload_to(self, filename):
             return 'organizations/%s/%s' % (self.name, filename)
