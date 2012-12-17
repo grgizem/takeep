e0bb37175c12bbfd36b644c88f098e173d416ffc
Kemal Akkoyun <kakkoyun@gmail.com>
2012-12-17 22:32:57 +0200

    Minor detail changes at Place model.

--- event/apps/place/models.py
+++ event/apps/place/models.py
@@ -2,9 +2,22 @@ from django.db import models
 
 
 class Place(models.Model):
-    name = models.CharField(max_length='100')
-    purpose = models.CharField(max_length='255')
-    coordinates = models.CharField(blank=True, max_length=100)
+    name = models.CharField("Place Name", max_length='100')
+    description = models.CharField("Description", max_length='255')
+    coordinates = models.CharField("Geolocation", blank=True, max_length=100)
+    address = models.CharField("Address",
+                               max_length=255,
+                               blank=True,
+                               null=True
+                               )
+    phone_number = models.CharField("Phone Number",
+                                    max_length=11,
+                                    blank=True,
+                                    null=True
+                                    )
+
+    email = models.EmailField("E-Mail", blank=True, null=True,)
+    webpage = models.URLField("Webpage", blank=True, null=True,)
 
     def upload_to(self, filename):
             return 'organizations/%s/%s' % (self.name, filename)
