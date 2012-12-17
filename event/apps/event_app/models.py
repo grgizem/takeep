aa592ad14724b79d0ee2d39106bbfdcede85b5ba
Kemal Akkoyun <kakkoyun@gmail.com>
2012-12-17 22:27:03 +0200

    New fields added to event model. It is better to keep access property of event as a boolean field since we have only two choices.

--- event/apps/event_app/models.py
+++ event/apps/event_app/models.py
@@ -9,7 +9,14 @@ class Event(models.Model):
     description = models.CharField(max_length='255')
     participant = models.ManyToManyField(User, related_name=u'attended_events')
     location = models.ForeignKey(Place, related_name='had_events')
-    access = models.IntegerField(default=0)
+    is_private = models.BooleanField(default=False)
+
+    tags = models.CharField(max_length=255,
+                            blank=True,
+                            null=True
+                            )
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
 
     def upload_to(self, filename):
             return 'events/%s/%s' % (self.title, filename)
