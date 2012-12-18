bcb49b77fe0a6696b491da48d71abae17bf4c5a0
Gizem Gür <grgizem@gmail.com>
2012-12-18 11:58:37 +0200

    url files added #TODO: adding regular expressions

---
+++ event/apps/event_app/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import patterns, url
+
+
+urlpatterns = patterns('apps.event_app.views',
+    url(r'^events/', 'events', name='events'),
+    url(r'^event/', 'event', name='event'),
+    url(r'^edit_event/', 'edit_event', name='edit_event'),
+    url(r'^create_event/', 'create_event', name='create_event'),
+)
