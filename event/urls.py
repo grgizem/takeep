18893228ebae7590188e1e0817484bbece1f6887
Gizem Gür <grgizem@gmail.com>
2012-12-18 12:02:16 +0200

    main url file written

--- event/urls.py
+++ event/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
-
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'event.views.home', name='home'),
-    # url(r'^event/', include('event.foo.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^account/', include('apps.account.urls')),
+    url(r'^event/', include('apps.event_app.urls')),
+    url(r'^place/', include('apps.place.urls')),
 )
