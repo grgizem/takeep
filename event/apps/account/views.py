ddc8cab70cdf75cb1d62de014f783b586bc12d9a
Gizem Gür <grgizem@gmail.com>
2012-12-18 11:57:32 +0200

    dummy views written

--- event/apps/account/views.py
+++ event/apps/account/views.py
@@ -1,1 +1,29 @@
-# Create your views here.
+from django.shortcuts import render
+
+
+def profile(request):
+    return render(request, 'profile.html')
+
+
+def edit_profile(request):
+    return render(request, 'edit_profile.html')
+
+
+def my_events(request):
+    return render(request, 'my_events.html')
+
+
+def my_event(request):
+    return render(request, 'my_event.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def logout(request):
+    return render(request, 'logout.html')
--- event/apps/event_app/views.py
+++ event/apps/event_app/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from django.shortcuts import render
+
+
+def events(request):
+    return render(request, 'events.html')
+
+
+def event(request):
+    return render(request, 'event.html')
+
+
+def create_event(request):
+    return render(request, 'create_event.html')
+
+
+def edit_event(request):
+    return render(request, 'edit_event.html')
