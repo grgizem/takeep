f956351e17e864538429ad083afbc34f9acd5f4e
ece <ecekivman@gmail.com>
2012-12-20 17:42:13 +0200

    edited urls

--- event/apps/event_app/urls.py
+++ event/apps/event_app/urls.py
@@ -11,9 +11,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^logout/$', logout, name='logout'),
-
-    url(r'^$','index'),
     
 )
--- event/apps/event_app/views.py
+++ event/apps/event_app/views.py
@@ -11,19 +11,17 @@ from event_app.models import Event, Participant
 from django.contrib.auth.models import User
 from social_auth.utils import setting
 
+#logout
 @login_required
 def logout(request):
     auth_logout(request)
     return HttpResponseRedirect("/")
 
 
-def home(request):
-    """Home view"""    
-    if request.user.is_authenticated():	
-        return render_to_response('profile.html', RequestContext(request))
-    else:
-	    return render_to_response('index.html', RequestContext(request))
-
+# profile page:
+@login_required
+def profile(request):
+    return render_to_response('profile.html', RequestContext(request))
 
 def events(request):
 	
@@ -38,5 +36,21 @@ def create_event(request):
     return render(request, 'create_event.html')
 
 
-def edit_event(request):
+#edit event
+@login_required
+def edit_event(request,event_id):
+    e = get_object_or_404(Event, id=event_id)
+    if request.user.id == e.host.id:
+        if request.POST:
+            eventform = EventForm(request.POST)
+            if eventform.is_valid():
+		eventform.save_as(request,e)
+		messages.add_message(request, messages.WARNING, 'Your post is waiting for approvement.')
+                return HttpResponseRedirect('/')
+        else:
+            eventform = EventForm({'title' : e.title, 'content' : e.content})
+            return render_to_response('edit_event.html', {'event_id' : event_id, 'form' : eventform}, RequestContext(request))
+    else:
+        messages.add_message(request, messages.ERROR, 'You can not edit this post, because you are not the host of it.')
+        return HttpResponseRedirect('/')
     return render(request, 'edit_event.html')
