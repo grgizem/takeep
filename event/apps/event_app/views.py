02504a56792a82b6a76b23f18e1f2ede26d1bb4c
ece <ecekivman@gmail.com>
2012-12-20 17:16:08 +0200

    edited forms

--- event/apps/event_app/views.py
+++ event/apps/event_app/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+from django.shortcuts import render_to_response, redirect, get_object_or_404
+from django.contrib.messages.api import get_messages
+
+import random
+
+from event_app.models import Event, Participant 
+from django.contrib.auth.models import User
+from social_auth.utils import setting
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return HttpResponseRedirect("/")
+
+
+def home(request):
+    """Home view"""    
+    if request.user.is_authenticated():	
+        return render_to_response('profile.html', RequestContext(request))
+    else:
+	    return render_to_response('index.html', RequestContext(request))
 
 
 def events(request):
+	
     return render(request, 'events.html')
 
 
--- event/dictionary/forms.py
+++ event/dictionary/forms.py
@@ -1,24 +1,36 @@
-mport datetime
-from django import forms
+import datetime
+from django.forms import ModelForm, Textarea
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.mail import send_mail
 from dictionary.models import Events, Participant
-from ajax_select.fields import AutoCompleteField
 
+class NewUserCreationForm(UserCreationForm):
+	email = forms.EmailField(required = True)
+	
+	class Meta:
+		model = User
+		fields = ("username","email","password")
+
+	def save(self):
+        	user = User.objects.create_user(self.cleaned_data["username"],self.cleaned_data["email"],self.cleaned_data["password"])
+		user.is_active=False
+		user.save()
+		
 class EventForm(forms.ModelForm):
     class Meta:
     	model = Event
     	exclude = ["host", "location", "is_private", "tags", "start_time", "end_time"]
 
     def save(self, request):
-        event = Event(title = self.cleaned_data["Title"])
-        event = Event(details = self.cleaned_data["Details"])
+        event = Event(title = self.cleaned_data["Title"], details = self.cleaned_data["Details"], host = request.user)
         event.save()
-        return event
     
     def saveAs(self, request, event):
 	event.title = self.cleaned_data["Title"]
 	event.details = self.cleaned_data["Details"]
 	event.save()
-	return event
+
 
 class ParticipantForm(forms.ModelForm):
     class Meta:
