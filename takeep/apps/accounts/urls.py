e2eb74e69d26c67d34ea2450ad5264d62dd2db1c
Gizem Gür <grgizem@gmail.com>
2012-12-22 21:14:18 +0200

    app name changed to accounts

---
+++ takeep/apps/accounts/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import patterns, url
+
+
+urlpatterns = patterns('apps.accounts.views',
+    url(r'^profile/', 'profile', name='profile'),
+    url(r'^edit_profile/', 'edit_profile', name='edit_profile'),
+    url(r'^my_events/', 'my_events', name='my_events'),
+    url(r'^my_event/', 'my_event', name='my_event'),
+
+    url(r'^login/', 'login', name='login'),
+    url(r'^register/', 'register', name='register'),
+    url(r'^logout/', 'logout', name='logout'),
+)
