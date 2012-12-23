9908c25b5e59b32a64edd55441e3ed66502b15a9
Gizem Gür <grgizem@gmail.com>
2012-12-23 20:27:14 +0200

    password reset and change urls added

--- takeep/apps/accounts/urls.py
+++ takeep/apps/accounts/urls.py
@@ -5,9 +5,36 @@ urlpatterns = patterns('apps.accounts.views',
     url(r'^profile/', 'profile', name='profile'),
     url(r'^edit_profile/', 'edit_profile', name='edit_profile'),
     url(r'^my_events/', 'my_events', name='my_events'),
-    url(r'^my_event/', 'my_event', name='my_event'),
+    url(r'^my_event/(?P<event_id>\d+)/$', 'my_event', name='my_event'),
 
     url(r'^login/', 'login', name='login'),
-    url(r'^register/', 'register', name='register'),
-    url(r'^logout/', 'logout', name='logout'),
+
+)
+
+urlpatterns += patterns('',
+    # to logout
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login',
+        name="logout"),
+
+    # to change the password for logged-in user
+    url(r'^password_change/$', 'django.contrib.auth.views.password_change',
+        {'template_name': 'accounts/password_change.html'},
+        name='password_change'),
+    url(r'^parola-degistirildi/$',
+        'django.contrib.auth.views.password_change_done',
+        {'template_name': 'accounts/password_change_done.html'},
+        name='password_change_done'),
+
+    # to reset the password for anonymous user
+    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset',
+        name='password_reset'),
+    url(r'^password_reset_done/$',
+        'django.contrib.auth.views.password_reset_done',
+        name='password_reset_done'),
+    url(r'^password_reset_confirm/(?P<uidb36>[-\w]+)/(?P<token>[-\w]+)/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        name='password_reset_confirm'),
+    url(r'^password_reset_complete/$',
+        'django.contrib.auth.views.password_reset_complete',
+        name='password_reset_complete'),
 )
