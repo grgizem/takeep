c1dff83aad1be5ace5c053f6a8b68c045865842d
Gizem Gür <grgizem@gmail.com>
2012-12-23 22:01:42 +0200

    event id added

--- takeep/apps/event/urls.py
+++ takeep/apps/event/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('apps.event.views',
 	url(r'^events/', 'events', name='events'),
 	url(r'^event/(?P<event_id>\d+)/$', 'event', name='event'),
-	url(r'^edit_event/', 'edit_event', name='edit_event'),
+	url(r'^edit_event/(?P<event_id>\d+)/$', 'edit_event', name='edit_event'),
 	url(r'^create_event/', 'create_event', name='create_event'),
 )
