e20fc56f5e6f056ce9dfe62541fc88d09c56b6c4
Gizem Gür <grgizem@gmail.com>
2012-12-24 01:56:52 +0200

    approvement urls added

--- takeep/apps/event/urls.py
+++ takeep/apps/event/urls.py
@@ -4,6 +4,11 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('apps.event.views',
 	url(r'^events/', 'events', name='events'),
 	url(r'^event/(?P<event_id>\d+)/$', 'event', name='event'),
+    url(r'^create_event/', 'create_event', name='create_event'),
+
 	url(r'^edit_event/(?P<event_id>\d+)/$', 'edit_event', name='edit_event'),
-	url(r'^create_event/', 'create_event', name='create_event'),
+    url(r'^cancel_event/(?P<event_id>\d+)/$', 'cancel_event', name='cancel_event'),
+
+    url(r'^approve/(?P<event_id>\d+)(?P<user_id>\d+)/$', 'approve', name='approve'),
+    url(r'^disapprove/(?P<event_id>\d+)(?P<user_id>\d+)/$', 'disapprove', name='disapprove'),
 )
