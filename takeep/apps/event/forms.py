80a72cec02e816001828ef3145f3a761b949ea76
Gizem Gür <grgizem@gmail.com>
2012-12-24 02:44:58 +0200

    status excluded

--- takeep/apps/event/forms.py
+++ takeep/apps/event/forms.py
@@ -6,11 +6,12 @@ from apps.event.models import Event
 class EventForm(ModelForm):
     class Meta:
         model = Event
-        exclude = ["host", ]
+        exclude = ["host", "status"]
 
     def save(self, user, commit=True):
         instance = super(EventForm, self).save(commit=False)
         instance.host = user
+        instance.status = "O"
         if commit:
             instance.save()
         return instance
