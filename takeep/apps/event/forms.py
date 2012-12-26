872183ab3beaf1197f9a9116cebb6d8023647dbd
Gizem Gür <grgizem@gmail.com>
2012-12-26 20:56:17 +0200

    report form added

--- takeep/apps/event/forms.py
+++ takeep/apps/event/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from apps.event.models import Event
+from apps.event.models import Event, EventReport
 
 
 class EventForm(forms.ModelForm):
@@ -15,6 +15,21 @@ class EventForm(forms.ModelForm):
         instance = super(EventForm, self).save(commit=False)
         instance.host = user
         instance.status = "O"
+        if commit:
+            instance.save()
+        return instance
+
+
+class EventReportForm(forms.ModelForm):
+
+    class Meta:
+        model = EventReport
+        exclude = ["user", "is_considered"]
+
+    def save(self, user, event, commit=True):
+        instance = super(EventReportForm, self).save(commit=False)
+        instance.user = user
+        instance.event = event
         if commit:
             instance.save()
         return instance
