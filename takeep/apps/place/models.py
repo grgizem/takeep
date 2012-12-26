c70c67840bcaaf539428e548565f3b077d1abc40
Gizem Gür <grgizem@gmail.com>
2012-12-26 20:56:57 +0200

    placeflag added

--- takeep/apps/place/models.py
+++ takeep/apps/place/models.py
@@ -54,3 +54,59 @@ class Place(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class FlagPlace(models.Model):
+    place = models.OneToOneField(Place)
+    """
+    The reported place to be change
+    """
+    name = models.CharField("Place Name", max_length='100')
+    """
+    The Name of the place.
+    """
+    description = models.CharField("Description", max_length='255')
+    """
+    The Description of the place.
+    """
+    coordinates = models.CharField("Geolocation", blank=True, max_length=100)
+    """
+    The Coordinates of the place which is a geographical representation as
+    longtitude and latitude.
+    """
+    address = models.CharField("Address",
+                               max_length=255,
+                               blank=True,
+                               null=True
+                               )
+    """
+    The Adress of the place which is an open presciriptive adress.
+    """
+    phone_number = models.CharField("Phone Number",
+                                    max_length=11,
+                                    blank=True,
+                                    null=True
+                                    )
+    """
+    The Phone number of the place.
+    """
+    email = models.EmailField("E-Mail", blank=True, null=True,)
+    """
+    The Email of the place.
+    """
+    webpage = models.URLField("Webpage", blank=True, null=True,)
+    """
+    The Webpage of the place.
+    """
+    is_approved = models.BooleanField(default=False)
+    """
+    The approval status of the place
+    """
+
+    def upload_to(self, filename):
+            return 'organizations/%s/%s' % (self.name, filename)
+
+    banner = models.ImageField(upload_to=upload_to)
+    """
+    The banner photo of the place.
+    """
