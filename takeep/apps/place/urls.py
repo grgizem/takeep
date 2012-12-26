e334561f387f6848c3e72bc5d4ed6f5cecf95982
Kemal Akkoyun <kakkoyun@gmail.com>
2012-12-26 18:10:42 +0200

    place initialized

--- takeep/apps/place/urls.py
+++ takeep/apps/place/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls.defaults import patterns, url
 
 
-urlpatterns = patterns('place.views',
+urlpatterns = patterns('apps.place.views',
+    url(r'^places/', 'places', name='places'),
+    url(r'^place/(?P<place_id>\d+)/$', 'place', name='place'),
+    url(r'^create_place/', 'create_place', name='create_place'),
+
+    url(r'^flag/(?P<place_id>\d+)/$', 'flag', name='flag'),
 )
--- takeep/apps/place/views.py
+++ takeep/apps/place/views.py
@@ -1,1 +1,23 @@
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+
+from apps.place.models import Place
+
+
+def places(request):
+    places = Place.objects.all()
+    return render(request, 'place/places.html', {'places': places})
+
+
+def place(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    events = place.had_events.all()
+    return render(request, 'place/place.html',
+        {'place': place, 'events': events})
+
+
+def create_place(request):
+    return render(request, 'place/create_place.html')
+
+
+def flag(request, place_id):
+    return render(request, 'place/flag.html')
