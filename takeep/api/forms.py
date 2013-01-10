0289f508e7157ac1814fabbf7d88ab40d456adc0
Kemal Akkoyun <kakkoyun@gmail.com>
2013-01-10 20:00:06 +0200

    Particapant validation form of api created.

--- takeep/api/forms.py
+++ takeep/api/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from apps.event.models import Participant
+
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
 
 
+class ParticipantForm(forms.ModelForm):
+    class Meta:
+        model = Participant
